b4b860f4c397fa85567c7affee4a6e50010d3c12
EduardoManczenko <eduardo.manczenko@gmail.com>
2023-04-29 20:26:41 -0300

    fix: search bar focus hover

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,16 +13,18 @@ export function Input({
     borderColor = "lightGray",
 }: Props){  
     return(
-        <input type="text" placeholder={placeHolder} className={classNames(
-            `placeholder-[#808080] border-2  rounded-md py-2 px-4 w-full h-[42px]`,
-            {
-                "border-[#EAEAEA]": borderColor === "lightGray"
-            },
-            {
-                "border-[#FFFFFF]": borderColor === "transparent"
-            },
-            
-            className
-        )}/>
+        <div>
+
+            <input type="text" placeholder={placeHolder} className={classNames(
+                `placeholder-[#808080] border-2 rounded-md py-2 px-4 w-full h-[42px]`,
+                {
+                    "border-[#EAEAEA]": borderColor === "lightGray"
+                },
+                {
+                    "border-[#FFFFFF] focus:outline-gray-200": borderColor === "transparent"
+                },
+                className
+            )}/>
+        </div>
     )
 }
