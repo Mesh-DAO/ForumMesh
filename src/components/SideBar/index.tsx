6b85655aadd3baddc998950a5a8bea6c55f65ff6
EduardoManczenko <eduardo.manczenko@gmail.com>
2023-04-30 00:57:07 -0300

    fix: sidebar footer responsive

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -14,14 +14,17 @@ import { Title } from "../Title";
 
 export function SideBar() {
   return (
-    <div className="fixed top-[130px] flex  flex-col justify-center w-[20%]">
+    <div className="fixed top-[130px] flex  flex-col justify-between h-[80%] w-[20%]">
+      
       <div className="flex flex-col w-full gap-4">
+
         <div className="flex items-center mb-5 pl-[10%]">
             <Search/>
             <Input placeHolder="Search" borderColor="transparent"  className="ml-2 flex-grow"/>
         </div>
         
         <div className="flex flex-col gap-6 w-[60%]">
+
             <div className="flex flex-col ">
                 <Title size="sm" color="gray" className="pl-[20%] mb-[10px]">MENU</Title>
                 <MenuItem><List/>Questions</MenuItem>
@@ -35,15 +38,17 @@ export function SideBar() {
                 <MenuItem><MessageCircle/>Answers</MenuItem>
                 <MenuItem><Heart/>Likes & Votes</MenuItem>
             </div>
+
         </div>
+
       </div>
-        <div className="fixed bottom-[10px] flex items-center justify-center w-[20%]">
-          <div className="flex flex-row gap-3 items-center justify-center">
-            <Github />
-            <Instagram />
-            <Facebook/>
-          </div>
-        </div>
+
+    <div className="flex flex-row gap-3 items-center justify-center">
+      <Github />
+      <Instagram />
+      <Facebook/>
+    </div>
+        
     </div>
   );
 }
